00ff3a92b0c30797c5267c38c3c41bfc70b616cd
minnymin3 <cmichael40@yahoo.com>
2014-04-20 16:43:16 -0400

    Stop casting on right click

--- Zephyrus-Core/src/main/java/net/minezrc/zephyrus/core/item/ItemListener.java
+++ Zephyrus-Core/src/main/java/net/minezrc/zephyrus/core/item/ItemListener.java
@@ -131,6 +131,11 @@ public class ItemListener implements Listener {
 				}
 			}
 		}
+		if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			if (user.isCastingSpell()) {
+				user.stopCasting();
+			}
+		}
 	}
 
 	public InventoryButton getButton(final Spell spell, final Block block, final Set<org.bukkit.entity.Item> entities) {
