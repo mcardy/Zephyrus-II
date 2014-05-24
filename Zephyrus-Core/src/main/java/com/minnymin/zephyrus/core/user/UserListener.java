ae8ddd9bd29f3092a18e9a742e9b712c93bb42b7
minnymin3 <cmichael40@yahoo.com>
2014-05-24 13:47:52 -0400

    Keep the display bar on world change and teleport

--- Zephyrus-Core/src/main/java/com/minnymin/zephyrus/core/user/UserListener.java
+++ Zephyrus-Core/src/main/java/com/minnymin/zephyrus/core/user/UserListener.java
@@ -1,8 +1,14 @@
 package com.minnymin.zephyrus.core.user;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.minnymin.zephyrus.Zephyrus;
 
 /**
  * Zephyrus - UserListener.java
@@ -24,6 +30,26 @@ public class UserListener implements Listener {
 		if (manager.userMap.containsKey(event.getPlayer().getName()))
 			return;
 		manager.userMap.put(event.getPlayer().getName(), new OnlineUser(event.getPlayer()));
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onTeleport(final PlayerTeleportEvent event) {
+		new BukkitRunnable() {
+			public void run() {
+				Zephyrus.getUserManager().getBarDisplay().removeBar(event.getPlayer());
+				Zephyrus.getUser(event.getPlayer()).drainMana(0);
+			}
+		}.runTaskLater(Zephyrus.getPlugin(), 10);
+	}
+	
+	@EventHandler
+	public void onWorldChange(final PlayerChangedWorldEvent event) {
+		new BukkitRunnable() {
+			public void run() {
+				Zephyrus.getUserManager().getBarDisplay().removeBar(event.getPlayer());
+				Zephyrus.getUser(event.getPlayer()).drainMana(0);
+			}
+		}.runTaskLater(Zephyrus.getPlugin(), 10);
 	}
 
 }
