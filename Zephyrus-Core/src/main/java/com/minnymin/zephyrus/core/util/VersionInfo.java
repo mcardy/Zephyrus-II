50ea3620c3d0a56658fb1608ac19892b5dc07f43
minnymin3 <cmichael40@yahoo.com>
2014-07-03 13:12:48 -0400

    Added language updating

--- Zephyrus-Core/src/main/java/com/minnymin/zephyrus/core/util/VersionInfo.java
+++ Zephyrus-Core/src/main/java/com/minnymin/zephyrus/core/util/VersionInfo.java
@@ -1,7 +1,8 @@
 package com.minnymin.zephyrus.core.util;
 
+import org.bukkit.plugin.Plugin;
+
 import com.minnymin.zephyrus.YmlConfigFile;
-import com.minnymin.zephyrus.Zephyrus;
 
 /**
  * Zephyrus - VersionInfo.java
@@ -12,14 +13,13 @@ import com.minnymin.zephyrus.Zephyrus;
 
 public class VersionInfo {
 
-	public static void load() {
+	public static void load(Plugin plugin) {
 		YmlConfigFile file = new YmlConfigFile("version.yml");
-		if (Zephyrus.getPlugin().getDescription().getVersion().contains("B:")) {
+		if (plugin.getDescription().getVersion().contains("B:")) {
 			try {
-				int version = Integer.parseInt(Zephyrus.getPlugin().getDescription().getVersion().split("B:")[1]);
+				int version = Integer.parseInt(plugin.getDescription().getVersion().split("B:")[1]);
 				if (file.getConfig().getInt("version") < version) {
-					Zephyrus.getPlugin()
-							.getLogger()
+					plugin.getLogger()
 							.warning(
 									"You have upgraded from one development version of Zephyrus to the next."
 											+ " Things may have changed and it is recommended that you delete spells.yml and locale.yml.");
@@ -29,10 +29,21 @@ public class VersionInfo {
 			} catch (Exception ex) {
 			}
 		} else {
-			if (file.getConfig().getString("release") != Zephyrus.getPlugin().getDescription().getVersion()) {
-				file.getConfig().set("release", Zephyrus.getPlugin().getDescription().getVersion());
+			if (file.getConfig().getString("release") != plugin.getDescription().getVersion()) {
+				String previous = file.getConfig().getString("release");
+				file.getConfig().set("release", plugin.getDescription().getVersion());
 				file.saveConfig();
+				updateLanguage(previous);
 			}
+		}
+	}
+	
+	/**
+	 * If language items have changed they will be updated here when the plugin updates
+	 */
+	private static void updateLanguage(String previousVersion) {
+		if (previousVersion == "2.0.0") {
+			//Language.remove("");
 		}
 	}
 }
